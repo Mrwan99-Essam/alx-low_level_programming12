dd27a325cc65d5104a7ded476d55ab533287fdc6
Mrwan99-Essam <mrwan99.essm@gmail.com>
2023-06-17 11:59:35 -0700

    update

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description:
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		putchar(digit + '0');
+		digit++
+	}
+	putchar('\n');
+
+	return (0);
+}
