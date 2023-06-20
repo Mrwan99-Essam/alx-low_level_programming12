801826ab637a98a333055e4b6b519dd78b17cc0d
Mrwan99-Essam <mrwan99.essm@gmail.com>
2023-06-20 07:39:19 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: prints _putchar
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}
