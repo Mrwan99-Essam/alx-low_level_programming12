3e357339912684aab5c94ac58ab07c13ba50dda4
Mrwan99-Essam <mrwan99.essm@gmail.com>
2023-06-22 09:23:48 -0700

    up

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints 0 - 9 only using _putchar twice
+ *
+ * Retunr: Always 0 (success)
+*/
+
+void print_numbers(void)
+{
+	int a
+
+	do {
+		_putchar(a + 48);
+		a++;
+	} while (a >= 0 && a <= 9);
+	_putchar('\n');
+}
