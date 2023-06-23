a36113c84f313d3fd5da20b753eeac502c5d82e0
Mrwan99-Essam <mrwan99.essm@gmail.com>
2023-06-22 19:10:49 -0700

    p

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,7 +12,7 @@ void print_square(int size)
 {
 	int row, colum;
 
-	for (row = 1; row <= size; colum++)
+	for (row = 1; row <= size; row++)
 	{
 		for (colum = 1; colum <= size; colum++)
 			_putchar('#');
