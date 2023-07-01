1ebf263a42f2ebc4c017bda561d02abd12ae91ed
Mrwan99-Essam <mrwan99.essm@gmail.com>
2023-07-01 05:06:14 -0700

    up

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,10 +1,26 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * *cap_string - capitalizes all words of a string
  * @s: input string
  * Return: string with capitalized words
 */
+
+int isLower(char)
+{
+	return (c >= 97 && c <= 122);
+}
+int isDelimiter(char c)
+{
+	int i;
+	char delimiter[] = " \t\n,.!?\"()()";
+
+	for (i = 0; i < 12; i++)
+		if (c == delimiter[i])
+			return (1);
+	return (0);
+}
 
 char *cap_string(char *s)
 {
