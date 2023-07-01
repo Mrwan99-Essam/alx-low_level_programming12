3917ccf6942a97571879cc6664751032f552792a
Mrwan99-Essam <mrwan99.essm@gmail.com>
2023-07-01 02:19:59 -0700

    up

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -15,11 +15,12 @@ char *_strncat(char *dest, char *src, int n)
 	int c, i;
 
 	c = 0;
+
 	while (dest[c])
 		c++;
-	for (i = 0; i < n && src[i] != '0'; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[c + i] = src[i];
-	dest[c + i] = '0';
+	dest[c + i] = '\0';
 
 	return (dest);
 }
