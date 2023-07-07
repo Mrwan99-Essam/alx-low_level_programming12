6a49f9c73b9f5b9f25cd4f4cb2dc4e68a3d8a8a5
Mrwan99-Essam <mrwan99.essm@gmail.com>
2023-07-07 02:10:44 -0700

    up

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * *_strncat - a function that concatenates two string
+ *
+ * @dest: pointer to destination
+ * @src: pointer to source input
+ * @n: most number of bytes from @src
+ *
+ * Return: @dest
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int c, i;
+
+	c = 0;
+
+	while (dest[c])
+		c++;
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[c + i] = src[i];
+	dest[c + i] = '\0';
+
+	return (dest);
+}
