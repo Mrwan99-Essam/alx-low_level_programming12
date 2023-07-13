6f8e01da46e11586ba40a3a06e0166a911799a5f
Mrwan99-Essam <mrwan99.essm@gmail.com>
2023-07-13 07:01:11 -0700

    ip

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * *_memset - filles memory with a constant bytes.
+ * @s: pointer tp puts the constant
+ * @b: constant
+ * @n: max bytes
+ * Return: s
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *ptr = s;
+
+	while (n--)
+		*s++;
+	return (ptr);
+}
+
+/**
+ * *_calloc - allocates memory for an array , using malloc
+ * @nmemb: array length
+ * @size: of ech elementa
+ * Return: pointer
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *m;
+
+	if (size == 0; || nmemb == 0)
+		return (NULL);
+	m = malloc(sizeof(int) * nmemb);
+
+	if (m == 0)
+		return (NULL);
+	_memset(m, 0, sizeof(int) * nmemb);
+
+	return (m);
+}
