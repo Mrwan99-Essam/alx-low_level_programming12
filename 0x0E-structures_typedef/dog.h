1984d320563bcd0595cea3edcbfa5bc602de414a
Mrwan99-Essam <mrwan99.essm@gmail.com>
2023-07-17 02:13:57 -0700

    up

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,25 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * Struct dog - a dog a stracutr
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owners' name
+ *
+ * Description: just a lone dog struct in a big kitty world
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
